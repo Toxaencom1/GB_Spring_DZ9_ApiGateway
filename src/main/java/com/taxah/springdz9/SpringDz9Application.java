b46e7df107e22cf3ebe536a5e9b65895460cfa63
Toxaencom1 <Toxaencom@gmail.com>
2024-02-09 14:17:13 +0600

    Add comments

--- src/main/java/com/taxah/springdz9/SpringDz9Application.java
+++ src/main/java/com/taxah/springdz9/SpringDz9Application.java
@@ -13,6 +13,14 @@ public class SpringDz9Application {
         SpringApplication.run(SpringDz9Application.class, args);
     }
 
+// Я так понял что это альтернативный метод настройки, т.к без него работает
+// та же настройка с теми же параметрами в application.yaml
+    /**
+     * Configures custom routes using Spring Cloud Gateway.
+     *
+     * @param builder The RouteLocatorBuilder for building routes.
+     * @return The configured RouteLocator.
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
